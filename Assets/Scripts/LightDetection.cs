b6c97440d87d3a7abba0007d0e408b4b03e6f3e3
Anne Barrett <anneb1397@yahoo.com>
2018-02-06 15:31:55 -0600

    Basic Lighting Reactions
    Sprites react to light source. Apply the new material,
    SpriteLightReaction to sprites for this effect. Basic reaction using
    colliders changes sprite color from white to red when in trigger area
    and vice versa.

---
+++ Assets/Scripts/LightDetection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDetection : MonoBehaviour {
+
+    
+	public void OnTriggerEnter2D(Collider2D c)
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    public void OnTriggerExit2D(Collider2D c)
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
