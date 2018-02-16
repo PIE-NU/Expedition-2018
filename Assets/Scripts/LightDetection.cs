80fdcd8cfe9a6570b7f78b1c3ab014545ad89fe4
AlexMartin4 <alexismartineau0@gmail.com>
2018-02-15 18:38:41 -0600

    Added Ignore Light layer
    Added Ignore Light layer for all objects that should not be lit.

--- Assets/Scripts/LightDetection.cs
+++ Assets/Scripts/LightDetection.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class LightDetection : MonoBehaviour {
 
-    
-	public void OnTriggerEnter2D(Collider2D c)
+    SpriteRenderer _spr;
+    GameObject player;
+
+    private void Start()
+    {
+        _spr = GetComponent<SpriteRenderer>();
+        player = GameObject.Find("player");
+    }
+
+    public void OnTriggerEnter2D(Collider2D c)
     {
         GetComponent<SpriteRenderer>().color = Color.red;
     }
@@ -23,5 +31,17 @@ public class LightDetection : MonoBehaviour {
     public void StopReact()
     {
 
+    }
+
+    private void Update()
+    {
+        if(player.GetComponent<SpriteRenderer>().sortingOrder < 30 + _spr.sortingOrder )//&& (player.transform.position.x < transform.position.x))
+        {
+            gameObject.layer = 9;
+        }
+        else
+        {
+            gameObject.layer = 8;
+        }
     }
 }
