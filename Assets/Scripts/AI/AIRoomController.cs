1b5f3dadcb4be0ee9251227ad42089ab52896819
Drew Kersnar <dakersnar@dhcp-10-105-102-201.wireless.northwestern.private>
2018-02-18 19:26:23 -0600

    Made AI defend a point

--- Assets/Scripts/AI/AIRoomController.cs
+++ Assets/Scripts/AI/AIRoomController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AIRoomController : MonoBehaviour {
 
 	List<AIFighter> m_aiFighters;
-
+	Fighter player;
 
 
 	void Start () {
@@ -15,8 +15,12 @@ public class AIRoomController : MonoBehaviour {
 		m_aiFighters = new List<AIFighter> ();
 		foreach (BasicMovement fighterObj in Object.FindObjectsOfType<BasicMovement>()) {
 			// Add Fighters that are not the current player
-			if (fighterObj.IsCurrentPlayer)
+			if (fighterObj.IsCurrentPlayer) {
+				player=fighterObj.GetComponent<Fighter> ();
 				continue;
+			}
+				
+				
 
 			// Collect fighter components of relevance
 			AIFighter fighterInfo = new AIFighter ();
@@ -30,11 +34,19 @@ public class AIRoomController : MonoBehaviour {
 	}
 	
 	void Update () {
-		Vector2 target = new Vector2 (0, 0);
+		Vector2 center = new Vector2 (1, 1);
+		Vector2 target = circle_based_target_finder(center, 1f);
 		foreach (AIFighter fighter in m_aiFighters) {
 			if (Vector2.Distance((Vector2)fighter.BasicMove.transform.position,target) < .1)
 				continue;
 			fighter.BasicMove.moveToPoint ((Vector3)target);
 		}
 	}
+	Vector2 circle_based_target_finder(Vector2 center, float radius) {
+		Vector2 x = player.transform.position;
+		Vector2 d = x-center;
+		d.Normalize ();
+		return center + (d * radius);
+	} 
+
 }
