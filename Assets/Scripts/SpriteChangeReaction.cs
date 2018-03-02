051db32ee115ea91ce5b8bf3de660c8407a8cfbb
Anne Barrett <anneb1397@yahoo.com>
2018-03-01 18:49:00 -0600

    Feature stationary lights (#19)
    * Create stationary light prefab
    
    -Stationary light prefab with inspector script to modify colors,
    intensity and range which then modifies the collider appropriately
    
    * Revert "Create stationary light prefab"
    
    This reverts commit 69d12559d519f700d1df786dd924410d64feea78.
    
    * Added SpriteChangeReactoin
    
    -Add script SpriteChangeReaction to objects to switch when in light
    -Must set the two sprites within inspector
    
    * Fixed player prefab
    
    -Fixed player prefab to remove extra lights

---
+++ Assets/Scripts/SpriteChangeReaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChangeReaction : LightDetection {
+
+    public Sprite SpriteInDarkness;
+    public Sprite SpriteInLight;
+
+    public override void React()
+    {
+        GetComponent<SpriteRenderer>().sprite = SpriteInLight;
+    }
+
+    public override void StopReact()
+    {
+        GetComponent<SpriteRenderer>().sprite = SpriteInDarkness;
+    }
+}
