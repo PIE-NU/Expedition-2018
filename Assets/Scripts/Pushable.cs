a42bc94e6966a0ddf689af33acfeb33fe2670353
AlexMartin4 <alexismartineau0@gmail.com>
2018-03-01 18:38:00 -0600

    Made the pushcart template into a prefab

--- Assets/Scripts/Pushable.cs
+++ Assets/Scripts/Pushable.cs
@@ -14,11 +14,10 @@ public class Pushable : MonoBehaviour {
     BasicMovement _bm;
     public bool draggable;
 
-    // Use this for initialization
+    // Use this for initializationC:\Users\alexi\Documents\GitHub\Expedition-2018\Assets\Scripts\BasicMovement.cs
     void Start() {
         //trigger_area = gameObject.GetComponentInChildren<Interactable>();
         first_trigger = true;
-        
 	}
 	
 	// Update is called once per frame
@@ -35,56 +34,60 @@ public class Pushable : MonoBehaviour {
         }
 
         //Assign the right direction 
-        if (trigger_area.gameObject.GetComponent<BoxCollider2D>().offset.y > 0)
+        if (trigger_area)
         {
-            facing_direction = "DOWN";
-        }
-        if (trigger_area.gameObject.GetComponent<BoxCollider2D>().offset.y < 0)
-        {
-            facing_direction = "UP";
-        }
-
-        if (trigger_area.gameObject.GetComponent<BoxCollider2D>().offset.x > 0)
-        {
-            facing_direction = "LEFT";
-        }
-        if (trigger_area.gameObject.GetComponent<BoxCollider2D>().offset.x < 0)
-        {
-            facing_direction = "RIGHT";
-        }
-        //-------------------------------
-        //Update the basic movement script
-        _bm.push_direction = facing_direction;
-        _bm.can_drag_item = draggable;
-
-        //Run when the player presses the interaction key
-        if (trigger_area.press_trigger && first_trigger)
-        {
-
-            offset = trigger_area.gameObject.GetComponent<Collider2D>().offset;
-            //Because of the perspective, the character has to be slightly futher away when pushing the block down
-            if(offset.y > 0)
+            if (trigger_area.gameObject.GetComponent<BoxCollider2D>().offset.y > 0)
             {
-                char_offset = new Vector3(offset.x * 2.5f, offset.y * 2.7f);
+                facing_direction = "DOWN";
             }
-            else
+            if (trigger_area.gameObject.GetComponent<BoxCollider2D>().offset.y < 0)
             {
-                char_offset = new Vector3(offset.x * 2.5f, offset.y * 1f);
+                facing_direction = "UP";
             }
-            
-            
-            trigger_area.actor.transform.position = transform.position + char_offset;
-            first_trigger = false;
-        }
-        if (trigger_area.press_trigger)
-        {
-            transform.position = -char_offset + trigger_area.actor.transform.position;
-        }
-        if (!trigger_area.press_trigger)
-        {
-            first_trigger = true;
-            facing_direction = "";
-            _bm.push_direction = "";
+
+            if (trigger_area.gameObject.GetComponent<BoxCollider2D>().offset.x > 0)
+            {
+                facing_direction = "LEFT";
+            }
+            if (trigger_area.gameObject.GetComponent<BoxCollider2D>().offset.x < 0)
+            {
+                facing_direction = "RIGHT";
+            }
+
+            //-------------------------------
+            //Update the basic movement script
+            _bm.push_direction = facing_direction;
+            _bm.can_drag_item = draggable;
+
+            //Run when the player presses the interaction key
+            if (trigger_area.press_trigger && first_trigger)
+            {
+
+                offset = trigger_area.gameObject.GetComponent<Collider2D>().offset;
+                //Because of the perspective, the character has to be slightly futher away when pushing the block down
+                if (offset.y > 0)
+                {
+                    char_offset = new Vector3(offset.x * 2.5f, offset.y * 2.7f);
+                }
+                else
+                {
+                    char_offset = new Vector3(offset.x * 2.5f, offset.y * 1f);
+                }
+
+
+                trigger_area.actor.transform.position = transform.position + char_offset;
+                first_trigger = false;
+            }
+            if (trigger_area.press_trigger)
+            {
+                transform.position = -char_offset + trigger_area.actor.transform.position;
+            }
+            if (!trigger_area.press_trigger)
+            {
+                first_trigger = true;
+                facing_direction = "";
+                _bm.push_direction = "";
+            }
         }
 	}
 }
