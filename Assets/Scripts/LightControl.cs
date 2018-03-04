0f78424bb956488e1625cfe375eb4fbf2752ac6f
Grygoree <GregoryLeung2016@u.northwestern.edu>
2018-03-04 16:51:32 -0600

    Feature switching equipment (#20)
    * split the lantern sprite sheet.
    
    followed the same settings as the flashlight sprite sheet
    
    * Got animations properly working for lantern mode
    
    Used larry's method, but did not adjust speed.
    
    * Prepared lighting test scene for a LightControl refactor
    
    Need to refactor LightControl.cs because it's difficult to plan out otherwise.
    
    * Refactored LightControl with no functionality changes.
    
    Turned a few repeated lines into functions
    
    * Did additional refactoring on LightControl
    
    Changed the start of the script so that flashlight starts on and lantern starts off
    
    * Added sprite switching and made it so that only one light can be equipped and active.
    
    Also did additional refactoring on LightControl.cs

--- Assets/Scripts/LightControl.cs
+++ Assets/Scripts/LightControl.cs
@@ -4,82 +4,151 @@ using UnityEngine;
 
 public class LightControl : MonoBehaviour {
 
+	private enum holdingLight
+	{
+		f,
+		l,
+		none
+	}
+
     public GameObject lantern, flashlight;
     public float lanternCharge, flashlightCharge;
     public KeyCode lanternKey, flashlightKey;
     private bool lanternOn, flashlightOn;
+	private holdingLight equippedLight;
+
     private float negateLantern = -1f, negateFlash = -1f;
     public float lanternFullCharge = 15f, flashlightFullCharge = 10f;
 
-   
+	private Fighter fighterSiblingScript;
 
-    // Use this for initialization
     void Start () {
-        //lanternOn = flashlightOn = false;
+		fighterSiblingScript = GetComponent<Fighter>();
+
         lanternCharge = lanternFullCharge;
         flashlightCharge = flashlightFullCharge;
 
-        lanternOn = lantern.activeInHierarchy;
-        flashlightOn = flashlight.activeInHierarchy;
+		changeEquippedLight(holdingLight.f);
+		turnOnFlashlight();
+		turnOffLantern();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 
-
+		//TODO: Possible bugged behavior if player presses lanternkey and flashlightkey on same frame
+		//      This might be fixed by putting it in a else if or a switch case.
         if (Input.GetKeyDown(lanternKey))
         {
-            if (lanternOn)
-            {
-                lantern.SetActive(false);
-                lanternOn = false;
-                negateLantern = 1f;
-            }
-            else
-            {
-                lantern.SetActive(true);
-                lanternOn = true;
-                negateLantern = -1f;
-            }
+			changeEquippedLight(holdingLight.l);
+			togglePlayerLightSource("lantern");
         }
 
         if (Input.GetKeyDown(flashlightKey))
         {
-            if (flashlightOn)
-            {
-                flashlight.SetActive(false);
-                flashlightOn = false;
-                negateFlash = 1f;
-            }
-            else
-            {
-                flashlight.SetActive(true);
-                flashlightOn = true;
-                negateFlash = -1f;
-            }
+			changeEquippedLight(holdingLight.f);
+			togglePlayerLightSource("flashlight");
         }
+			
+		//Running out of lantern charge
         if (lanternCharge >= lanternFullCharge && !lanternOn)
             lanternCharge = lanternFullCharge;
         else if (lanternCharge <= 0f && lanternOn)
         {
-            lanternOn = false;
-            lantern.SetActive(false);
-            negateLantern = 1f;
+			turnOffLantern();
         }
         else
             lanternCharge += Time.deltaTime * negateLantern;
         
-
+		//Running out of flashlight charge
         if (flashlightCharge >= flashlightFullCharge && !flashlightOn)
             flashlightCharge = flashlightFullCharge;
         else if (flashlightCharge <= 0f && flashlightOn)
         {
-            flashlightOn = false;
-            flashlight.SetActive(false);
-            negateFlash = 1f;
+			turnOffFlashlight();
         }
         else
             flashlightCharge += Time.deltaTime * negateFlash;
         
     }
+		
+	//This function only handles the act of turning a light on and off
+	private void togglePlayerLightSource(string playerLight)
+	{
+		switch (playerLight)
+		{
+		case "lantern":
+			turnOffFlashlight();
+			if (lanternOn)
+			{
+				turnOffLantern();
+			} else {
+				turnOnLantern();
+			}
+			break;
+
+		case "flashlight":
+			turnOffLantern();
+			if (flashlightOn)
+			{
+				turnOffFlashlight();
+			} else {
+				turnOnFlashlight();
+			}
+			break;
+
+		default:
+			Debug.Log ("togglePlayerLightSource was called with an invalid playerLight: " + playerLight);
+			break;
+		}
+	}
+
+	private void turnOnFlashlight()
+	{
+		flashlight.SetActive(true);
+		flashlightOn = true;
+		negateFlash = -1f;
+	}
+
+	private void turnOffFlashlight()
+	{
+		flashlight.SetActive(false);
+		flashlightOn = false;
+		negateFlash = 1f;
+	}
+
+	private void turnOnLantern()
+	{
+		lantern.SetActive(true);
+		lanternOn = true;
+		negateLantern = -1f;
+	}
+
+	private void turnOffLantern()
+	{
+		lantern.SetActive(false);
+		lanternOn = false;
+		negateLantern = 1f;
+	}
+
+	//This function handles switching the character sprite and any game manager hooks for equipment
+	private void changeEquippedLight(holdingLight e)
+	{
+		if (e != equippedLight)
+		{
+			equippedLight = e;
+			switch (e)
+			{
+			case holdingLight.f:
+				fighterSiblingScript.IdleAnimation = "f_idle";
+				fighterSiblingScript.WalkAnimation = "f_walk";
+				break;
+			case holdingLight.l:
+				fighterSiblingScript.IdleAnimation = "l_idle";
+				fighterSiblingScript.WalkAnimation = "l_walk";
+				break;
+			default:
+				break;
+			}
+		}
+	}
 }
