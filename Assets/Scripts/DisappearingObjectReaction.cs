197b25de56f7edb57cbef268b15826a6114aad83
Anne Barrett <anneb1397@yahoo.com>
2018-03-03 20:39:57 -0600

    Created Disappearing reaction to light
    -DisappearingObjectReaction script causes object to disappear and be
    passable but only with player's flashlight (currently)
    -Added a LightColliders layer

---
+++ Assets/Scripts/DisappearingObjectReaction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingObjectReaction : LightDetection {
+
+    private SpriteRenderer _spr;
+    private Collider2D _collider;
+    private Collider2D _lightCollider;
+
+    void Start()
+    {
+        _spr = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<Collider2D>();
+        _lightCollider = GameObject.Find("Flashlight").GetComponentInChildren<Collider2D>();
+    }
+
+    public void Update()
+    {
+        //_collider.IsTouchingLayers(10)
+        if (!_collider.IsTouching(_lightCollider))
+            StopReact();
+        
+    }
+
+    public override void React()
+    {
+        _collider.isTrigger = true;
+        _spr.enabled = false;
+    }
+
+    public override void StopReact()
+    {
+        _collider.isTrigger = false;
+        _spr.enabled = true;
+    }
+}
