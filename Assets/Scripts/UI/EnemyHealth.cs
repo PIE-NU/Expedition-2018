1b227c6cadc2bbe2ac7a9abcde5c7fc9a1ff036f
Brendan Labadie <brendanlabadie@u.northwestern.edu>
2018-03-10 16:25:05 -0600

    Added enemy healthbar

---
+++ Assets/Scripts/UI/EnemyHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EnemyHealth : MonoBehaviour {
+
+	public Image healthBar;
+
+	Attackable enemy;
+	public float currentHealth;
+	//bool isDead;
+	//bool damaged;
+
+	void Start () {
+		//healthSlider = GetComponentInChildren<Slider> ();
+		healthBar = GameObject.Find("HealthBar").GetComponentInChildren<Image> ();
+		foreach (BasicMovement playerObj in Object.FindObjectsOfType<BasicMovement>()) {
+			if (playerObj.IsCurrentPlayer) {
+				enemy = playerObj.GetComponent<Attackable>();
+			}
+		}
+	}
+
+	void Update () {
+		CheckEnemyHealth ();
+		UpdateHealthUI ();
+	}
+
+	void CheckEnemyHealth () {
+		currentHealth = enemy.Health;
+		//isDead = player.alive;
+		//damaged = currentHealth < oldHealth;
+	}
+
+	void UpdateHealthUI () {
+		healthBar.fillAmount = currentHealth/100f;
+	}
+}
