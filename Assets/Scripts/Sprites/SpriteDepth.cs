d791af87aff0b7c86296fa2f3b9ca52c2b1c3aab
Grygoree <GregoryLeung2016@u.northwestern.edu>
2018-03-11 15:40:53 -0500

    Reformat SpriteDepth.cs
    Also made m_spr private

--- Assets/Scripts/Sprites/SpriteDepth.cs
+++ Assets/Scripts/Sprites/SpriteDepth.cs
@@ -2,50 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof (SpriteRenderer))]
-public class SpriteDepth : MonoBehaviour {
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteDepth : MonoBehaviour
+{
 
 	public float Y_SortOffset = 0;
 	public bool AutoOffset = true;
 	public bool CastShadows = true;
 	public bool ReceiveShadows = true;
 	public bool AutoGenerateSideShadows = false;
-	SpriteRenderer m_spr;
+	private SpriteRenderer m_spr;
 
-	void Start () {
-		m_spr = GetComponent<SpriteRenderer> ();
-		GetComponent<Renderer> ().receiveShadows = ReceiveShadows;
+	void Start()
+	{
+		m_spr = GetComponent<SpriteRenderer>();
+		GetComponent<Renderer>().receiveShadows = ReceiveShadows;
 		if (CastShadows)
-			GetComponent<Renderer> ().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-		if (AutoGenerateSideShadows && GetComponent<BoxCollider2D>() && GetComponent<SpriteRenderer>()) {
+			GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+		if (AutoGenerateSideShadows && GetComponent<BoxCollider2D>() && GetComponent<SpriteRenderer>())
+		{
 
-			BoxCollider2D bc = GetComponent<BoxCollider2D> ();
-			GameObject t = Instantiate (FindObjectOfType<GameManager> ().SideShadowTemplate, transform);
-			t.GetComponent<SpriteRenderer> ().sprite = m_spr.sprite;
+			BoxCollider2D bc = GetComponent<BoxCollider2D>();
+			GameObject t = Instantiate(FindObjectOfType<GameManager>().SideShadowTemplate, transform);
+			t.GetComponent<SpriteRenderer>().sprite = m_spr.sprite;
 			Vector3 sc = transform.localScale;
 			//Debug.Log (sc);
-			t.transform.Translate (new Vector3 (sc.x * bc.size.x / 2, 0f,  sc.y * (bc.size.y/2f)));
-			t.transform.localScale = new Vector3 (sc.y * (bc.size.y / bc.size.x), 1f,1f);
-			t.transform.Rotate (0f, 90f, 0f);
+			t.transform.Translate(new Vector3(sc.x * bc.size.x / 2, 0f, sc.y * (bc.size.y / 2f)));
+			t.transform.localScale = new Vector3(sc.y * (bc.size.y / bc.size.x), 1f, 1f);
+			t.transform.Rotate(0f, 90f, 0f);
 			//Debug.Log (t);
 
-			GameObject t2 = Instantiate (FindObjectOfType<GameManager> ().SideShadowTemplate, transform);
-			t2.GetComponent<SpriteRenderer> ().sprite = m_spr.sprite;
-			t2.transform.Translate (new Vector3 (sc.x * -bc.size.x / 2, 0f, sc.y * (bc.size.y/2f)));
-			t2.transform.localScale = new Vector3 (sc.y * (bc.size.y / bc.size.x), 1f,1f);
-			t2.transform.Rotate (0f, 90f, 0f);
+			GameObject t2 = Instantiate(FindObjectOfType<GameManager>().SideShadowTemplate, transform);
+			t2.GetComponent<SpriteRenderer>().sprite = m_spr.sprite;
+			t2.transform.Translate(new Vector3(sc.x * -bc.size.x / 2, 0f, sc.y * (bc.size.y / 2f)));
+			t2.transform.localScale = new Vector3(sc.y * (bc.size.y / bc.size.x), 1f, 1f);
+			t2.transform.Rotate(0f, 90f, 0f);
 			//Debug.Log (t2);
 
-			GameObject t3 = Instantiate (FindObjectOfType<GameManager> ().SideShadowTemplate, transform);
-			t3.GetComponent<SpriteRenderer> ().sprite = m_spr.sprite;
-			t3.transform.Translate (new Vector3 (0f, 0f, sc.y * bc.size.y));
+			GameObject t3 = Instantiate(FindObjectOfType<GameManager>().SideShadowTemplate, transform);
+			t3.GetComponent<SpriteRenderer>().sprite = m_spr.sprite;
+			t3.transform.Translate(new Vector3(0f, 0f, sc.y * bc.size.y));
 			//t3.transform.localScale = new Vector3 (sc.x , sc.y, sc.z);
 			//Debug.Log (t3);
 		}
-		if (AutoOffset && GetComponent<BoxCollider2D>()) {
+		if (AutoOffset && GetComponent<BoxCollider2D>())
+		{
 			Vector3 sc = transform.localScale;
-			BoxCollider2D bc = GetComponent<BoxCollider2D> ();
-			Y_SortOffset =  (bc.offset.y - Mathf.Abs(bc.size.y/2f)) * sc.y;
+			BoxCollider2D bc = GetComponent<BoxCollider2D>();
+			Y_SortOffset = (bc.offset.y - Mathf.Abs(bc.size.y / 2f)) * sc.y;
 			/*if (sc.y < 1f) {
 				Y_SortOffset += (1f - sc.y) * 1.5f;
 			} else {
@@ -56,9 +60,10 @@ public class SpriteDepth : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		//m_spr.sortingOrder = Mathf.CeilToInt(transform.position.y * 100f) * -1 + Y_SortOffset;
 		Vector3 pos = m_spr.transform.position;
-		m_spr.transform.position = new Vector3 (pos.x, pos.y, pos.y + Y_SortOffset);
+		m_spr.transform.position = new Vector3(pos.x, pos.y, pos.y + Y_SortOffset);
 	}
 }
