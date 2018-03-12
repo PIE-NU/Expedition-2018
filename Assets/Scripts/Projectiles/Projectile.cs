12f9579a9fc1d842d0caa5031c02eb5bee90d3b8
Drew Kersnar <dakersnar@dhcp-10-105-99-213.wireless.northwestern.private>
2018-03-11 19:34:25 -0500

    Added collision management to the Projectile.cs script

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -5,10 +5,42 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
 
 	private Hitbox m_hitbox;
+	public float timealive=15f;
+
 
 	void Start()
 	{
 		m_hitbox = GetComponent<Hitbox>();
 		m_hitbox.Init();
+		StartCoroutine(DeathTime());
 	}
+
+	void Update()
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.GetComponent<Attackable>() != null) 
+		{
+			if (!col.gameObject.GetComponent<BasicMovement>().IsCurrentPlayer) 
+			{
+				col.gameObject.GetComponent<Attackable>().TakeHit(m_hitbox);
+				Destroy (gameObject);
+			}
+				
+		}
+		else
+			Destroy (gameObject);
+
+	}
+
+	private IEnumerator DeathTime()
+	{
+		yield return new WaitForSeconds (timealive);
+
+		Destroy (gameObject);
+	}
+
 }
