332f4ce85f3a48cc61786a3edec44201a74d0f1d
Grygoree <GregoryLeung2016@u.northwestern.edu>
2018-04-16 18:19:31 -0500

    working script

--- Assets/Scripts/Props/GameManagement/AudioManager.cs
+++ Assets/Scripts/Props/GameManagement/AudioManager.cs
@@ -6,22 +6,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour {
 
-	private AudioSource m_musicSource;
+	private GameObject m_music;
 
-	// Use this for initialization
-	void Start () {
-		m_musicSource = GameObject.Find("GameMusic");
-		if (m_musicSource == null) {
-			//panic
-			Debug.Log("Audio manager unable to find GameMusic");
-		}
+	void Awake() {
+		SceneManager.sceneLoaded += OnSceneLoaded;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+		GameObject newSceneMusicObj = GameObject.Find("GameMusic");
+
+		if (!newSceneMusicObj)
+		{
+			Debug.Log("nothing found");
+			return;
+		}
+
+		if (m_music == null) {
+
+			Debug.Log("first music");
+			//If music doesn't exist yet, replace it with new one
+			m_music = newSceneMusicObj;
+
+			//rename to avoid problems with already existing prefabs in later scenes
+			m_music.name = "CurrentMusic";
+			DontDestroyOnLoad(m_music);
+		} else {
+
+			Debug.Log("Checking music");
+			//Else check if the new music is the same as the current scene's music
+			AudioClip oldClip = m_music.GetComponent<AudioSource>().clip;
+			AudioClip newClip = newSceneMusicObj.GetComponent<AudioSource>().clip;
+
+			if(oldClip == newClip){
+				Debug.Log("REWIND REWIND REWIND");
+				Destroy(newSceneMusicObj);
+			} else {
+				Debug.Log("REPLACING");
+				Destroy(m_music);
+				m_music = newSceneMusicObj;
+				DontDestroyOnLoad(m_music);
+			}
+		}
 	}
 }
