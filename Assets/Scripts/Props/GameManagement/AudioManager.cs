519722020efcc2a32da49ab44c876cd1b2e8a303
Grygoree <GregoryLeung2016@u.northwestern.edu>
2018-04-16 16:35:21 -0500

    Added audio manager script

---
+++ Assets/Scripts/Props/GameManagement/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Singleton audio manager intended to be persistent across all scenes.
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	private AudioSource m_musicSource;
+
+	// Use this for initialization
+	void Start () {
+		m_musicSource = GameObject.Find("GameMusic");
+		if (m_musicSource === null) {
+			//panic
+			Debug.Log("Audio manager unable to find GameMusic");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
